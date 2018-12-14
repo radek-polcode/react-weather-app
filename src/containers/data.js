29cc57fe6fb152e3174a3fbafb97fe87910e3003
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-14 12:46:31 +0100

    Add data.js with weekly data

---
+++ src/containers/data.js
@@ -0,0 +1,141 @@
+const jsonData = {
+  "data": {
+    "city": {
+      "current_day": "friday",
+      "main_info": {
+        "name": "Barcelona",
+        "country": "Spain"
+      },
+      "weather": {
+        "current_temperature": "13",
+        "precipitation": "10%",
+        "humidity": "78%",
+        "wind": "11km/h"
+      },
+      "weekly_forecast": {
+        "friday": {
+          "description": "Partly Cloudy",
+          "temperature": {
+            "max": "13",
+            "min": "6",
+            "hourly": [
+              "11","13","13","12","9","8","7","7","7"
+            ]
+          },
+          "conditions": {
+            "precipitation": "0%",
+            "humidity": "74%",
+            "wind": "16km/h"
+          }
+        },
+        "saturday": {
+          "description": "Partly Cloudy",
+          "temperature": {
+            "max": "13",
+            "min": "6",
+            "hourly": [
+              "8","7","7","7","11","13","12","10","10"
+            ]
+          },
+          "conditions": {
+            "precipitation": "0%",
+            "humidity": "73%",
+            "wind": "16km/h"
+          }
+        },
+        "sunday": {
+          "description": "Scattered Showers",
+          "temperature": {
+            "max": "13",
+            "min": "6",
+            "hourly": [
+              "10","10","9","10","13","14","13","12","11"
+            ]
+          },
+          "conditions": {
+            "precipitation": "50%",
+            "humidity": "77%",
+            "wind": "18km/h"
+          }
+        },
+        "monday": {
+          "description": "Mostly Sunny",          
+          "temperature": {
+            "max": "13",
+            "min": "6",
+            "hourly": [
+              "11","9","8","8","12","14","11","8","7"
+            ]
+          },
+          "conditions": {
+            "precipitation": "10%",
+            "humidity": "69%",
+            "wind": "16km/h"
+          }
+        },
+        "tuesday": {
+          "description": "Partly Cloudy",
+          "temperature": {
+            "max": "13",
+            "min": "6",
+            "hourly": [
+              "7","6","6","6","12","14","12","12","12"
+            ]
+          },
+          "conditions": {
+            "precipitation": "0%",
+            "humidity": "73%",
+            "wind": "14km/h"
+          }
+        },
+        "wednesday": {
+          "description": "Cloudy",
+          "temperature": {
+            "max": "13",
+            "min": "6",
+            "hourly": [
+              "12","11","10","10","13","13","12","11","9"
+            ]
+          },
+          "conditions": {
+            "precipitation": "20%",
+            "humidity": "83%",
+            "wind": "14km/h"
+          }
+        },
+        "thursday": {
+          "description": "Scattered Showers",
+          "temperature": {
+            "max": "13",
+            "min": "6",
+            "hourly": [
+              "9","8","8","8","11","12","11","9","8"
+            ]
+          },
+          "conditions": {
+            "precipitation": "40%",
+            "humidity": "75%",
+            "wind": "13km/h"
+          }
+        },
+        "friday_next": {
+          "description": "Partly Cloudy",
+          "temperature": {
+            "max": "13",
+            "min": "6",
+            "hourly": [
+              "8","7","7","8","11","13","11","9","8"
+            ]
+          },
+          "conditions": {
+            "precipitation": "0%",
+            "humidity": "74%",
+            "wind": "16km/h"
+          }
+        }
+      }
+    }
+  }
+}
+
+export default jsonData;
