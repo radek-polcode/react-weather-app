1b60ef408eb7e4218fd9d9bd42e9a7e13020e9fb
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-14 10:47:48 +0100

    Add bootstrap, and create components structure

---
+++ src/components/WeatherBlock/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import './index.css';
+
+export default class extends Component {
+  render() {
+    return(
+      <div className="weather-block">
+        <h3>Weather block</h3>
+      </div>
+    )
+  }
+}
---
+++ src/containers/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import WeatherBlock from '../components/WeatherBlock';
+import Header from '../components/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Container>
+          <WeatherBlock />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
