5440e39f6a0c9ee09b6974f64f0541a7f6f80dda
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-14 13:39:15 +0100

    Add css styling to weather block

--- src/components/WeatherBlock/index.jsx
+++ src/components/WeatherBlock/index.jsx
@@ -2,12 +2,21 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import './index.css';
+import json_data from '../../containers/data.js';
 
 export default class extends Component {
+  state = { weather_data: json_data }
+
+  static propTypes = {}
+  static defaultProps = {}
+
+  constructor(props) {
+    super();
+  }
+
   render() {
     return(
       <div className="weather-block">
-        <h3>Weather block</h3>
       </div>
     )
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherBlock from '../components/WeatherBlock';
