aa9a7d568be385a4da1840d2603e5e5b5d248b04
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-17 10:18:28 +0100

    Updates

---
+++ precache-manifest.f879f1ff2d8a0e921c9469f1504139dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3757023ec49f9362201d",
+    "url": "/react-weather-app/static/css/main.008c1f94.chunk.css"
+  },
+  {
+    "revision": "3757023ec49f9362201d",
+    "url": "/react-weather-app/static/js/main.3757023e.chunk.js"
+  },
+  {
+    "revision": "047ea245f783486557b9",
+    "url": "/react-weather-app/static/css/1.55cedbdf.chunk.css"
+  },
+  {
+    "revision": "047ea245f783486557b9",
+    "url": "/react-weather-app/static/js/1.047ea245.chunk.js"
+  },
+  {
+    "revision": "72a6b7ef5541b624727a",
+    "url": "/react-weather-app/static/js/runtime~main.72a6b7ef.js"
+  },
+  {
+    "revision": "222687688b9e6e38f82d6e010e22f86a",
+    "url": "/react-weather-app/index.html"
+  }
+];
