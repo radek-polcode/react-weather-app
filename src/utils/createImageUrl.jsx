1eff3c59d6b4f329c104ac165186af0008b76d34
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-19 08:43:40 +0100

    remove unnecessary object

--- src/utils/createImageUrl.jsx
+++ src/utils/createImageUrl.jsx
@@ -1,9 +1,5 @@
 export const createImageUrl = function(size, description)
 { 
-  const mappings = {
-    'scattered_showers': 'rain_light',
-    'mostly_sunny': 'sunny'
-  }
   let imageUrl = "https://ssl.gstatic.com/onebox/weather/_size_/_type_.png"
   let type = createType(description);
 
