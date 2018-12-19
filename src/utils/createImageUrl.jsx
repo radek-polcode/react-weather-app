f7f2be9fa266f6416d0f2a64c1aa79ff8dd5612f
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-19 08:33:19 +0100

    Update function to create image url

--- src/utils/createImageUrl.jsx
+++ src/utils/createImageUrl.jsx
@@ -1,8 +1,20 @@
 export const createImageUrl = function(size, description)
 { 
+  const mappings = {
+    'scattered_showers': 'rain_light',
+    'mostly_sunny': 'sunny'
+  }
   let imageUrl = "https://ssl.gstatic.com/onebox/weather/_size_/_type_.png"
   let type = createType(description);
-  return imageUrl.replace('_size_', size).replace('_type_', type)
+
+  switch(type) {
+    case 'scattered_showers':
+      return imageUrl.replace('_size_', size).replace('_type_', 'rain_light')
+    case 'mostly_sunny':
+      return imageUrl.replace('_size_', size).replace('_type_', 'sunny')
+    default:
+     return imageUrl.replace('_size_', size).replace('_type_', type)
+  }
 }
 
 function createType(description) {
