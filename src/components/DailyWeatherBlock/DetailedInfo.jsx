6bdaefbc737fe634cf147b93839c18230079d97e
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-12-31 12:06:56 +0100

    Change DetailedInfo to class component

--- src/components/DailyWeatherBlock/DetailedInfo.jsx
+++ src/components/DailyWeatherBlock/DetailedInfo.jsx
@@ -1,54 +1,57 @@
-import React from 'react';
+import React , { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button, ButtonGroup, Col, Row } from 'reactstrap';
 
 import { createImageUrl } from '../../utils/createImageUrl';
 import './DetailedInfo.css';
 
-DetailedInfo.propTypes = {
-  currentDateTimeWeather: PropTypes.object.isRequired
+export default class DetailedInfo extends Component {
+  state = {
+  }
+  
+  static propTypes = {
+    currentDateTimeWeather: PropTypes.object.isRequired
+  }
+  static defaultProps = {
+    currentDateTimeWeather: {}
+  }
+
+  render() {
+    let currentDateTimeWeather = this.props.currentDateTimeWeather
+    let humidity = currentDateTimeWeather['humidity']
+    let imgCode = currentDateTimeWeather['imgCode']
+    let pressure = currentDateTimeWeather['pressure']
+    let temperature = currentDateTimeWeather['temperature']
+    let wind = currentDateTimeWeather['wind']
+
+    return (
+      <Row className="detailedInfo">
+        <Col xs="6">
+          <img className="detailedInfo__image" 
+              src={createImageUrl(imgCode)}
+              alt="weather-thumbnail"
+          >
+          </img>
+          <div className="detailedInfo__temperatureBlock">
+            <span className="detailedInfo__currentTemperature">{temperature}</span>
+            <span className="detailedInfo__units">&deg;C | &deg;F</span>
+          </div>
+        </Col>
+        <Col xs="6">
+          <div className="detailedInfo__details">    
+            <p>Pressure: {pressure} hPa</p>
+            <p>Humidity: {humidity} %</p>
+            <p>Wind: {wind} mps</p>
+            <ButtonGroup>
+              <Button size="sm">
+                Temperature
+              </Button>
+              <Button size="sm">Precipitation</Button>
+              <Button size="sm">Wind</Button>
+            </ButtonGroup>
+          </div>
+        </Col>
+      </Row>
+    )
+  }
 }
-
-function DetailedInfo({ currentDateTimeWeather }) {
-  //api
-  const humidity = currentDateTimeWeather['humidity']
-  const imgCode = currentDateTimeWeather['imgCode']
-  const pressure = currentDateTimeWeather['pressure']
-  const temperature = currentDateTimeWeather['temperature']
-  const wind = currentDateTimeWeather['wind']
-
-  return (
-    <Row className="detailedInfo">
-      <Col xs="6">
-        <img className="detailedInfo__image" 
-             src={createImageUrl(imgCode)}
-             alt="weather-thumbnail"
-        >
-        </img>
-        <div className="detailedInfo__temperatureBlock">
-          <span className="detailedInfo__currentTemperature">{temperature}</span>
-          <span className="detailedInfo__units">&deg;C | &deg;F</span>
-        </div>
-      </Col>
-      <Col xs="6">
-        <div className="detailedInfo__details">    
-          <p>Pressure: {pressure} hPa</p>
-          <p>Humidity: {humidity} %</p>
-          <p>Wind: {wind} mps</p>
-          <ButtonGroup>
-            <Button size="sm">Temperature</Button>
-            <Button size="sm">Precipitation</Button>
-            <Button size="sm">Wind</Button>
-          </ButtonGroup>
-        </div>
-      </Col>
-    </Row>
-  )
-}
-
-DetailedInfo.propTypes = {
-
-}
-
-export default DetailedInfo
-
